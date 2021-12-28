65db8036977e7fbd8bfdade41dd293a0e5ef9745
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-28 17:41:04 +0530

    variables

---
+++ variable1.py
@@ -0,0 +1,7 @@
+students_count = 1000
+print(students_count)
+is_published = False
+course_name = "Python Programming"
+print(course_name)
+rating = 4.89
+print(rating)
---
+++ variable2.py
@@ -0,0 +1,11 @@
+course = "Python Programming"
+message = """
+east n west, cats are the best!🐈
+"""
+print(message)
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[1:4])  # including 1, excluding 4
+print(course[3:])  # erases before 3
+print(course[:5])
