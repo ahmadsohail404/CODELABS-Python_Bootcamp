9e7eabadabc9020dcc545c9b61dc23ef4765eaf4
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-30 02:30:14 +0530

    chaining comparison operator

---
+++ Control Flow/chainingComparison.py
@@ -0,0 +1,6 @@
+# age should be between 18 and 65
+age = 22
+# if age >= 18 and age < 65:
+# #-> both statements are equivalent
+if 18 <= age < 65:      # -> chaining comparison operators
+    print("Eligible")
