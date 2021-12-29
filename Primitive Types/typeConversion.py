a55ffcd6912e6f8c89396c8555b5b9e2be5016e2
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-29 16:52:31 +0530

    type conversion

---
+++ Primitive Types/typeConversion.py
@@ -0,0 +1,7 @@
+x = input("x: ")
+# y = x + 1
+# don't run it using code runner because it's read only so can't take input
+
+# here we need to convert the string1 to a number and
+y = int(x) + 1
+print(f"x is: {x}, y is: {y}")
