d8e7f39f7c2ce5faa346d07c15df6e496379af82
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-29 16:31:49 +0530

    math module

---
+++ Primitive Types/numbersFunctions.py
@@ -0,0 +1,8 @@
+import math     # here math is an object
+
+print(round(2.8))
+print(abs(-24))
+
+print(math.ceil(2.2))      # returns >= the number
+
+# read more about math module from documentation
