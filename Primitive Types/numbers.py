accac2a5a498a77e21a3bd4716cb4e38489256e5
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-29 16:21:28 +0530

    numbers

---
+++ Primitive Types/numbers.py
@@ -0,0 +1,18 @@
+x = 1
+y = 1.34
+z = x + 2j  # a + ib
+# instead of i we use j for complex numbers
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)  # 10 to the power 3
+
+# Augmented assignment operator
+x = 10
+
+# x = x+3
+x += 3
+print(x)
