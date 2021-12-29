ef4fc1cda27f144516cb212226207e1bb0aa818b
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-30 02:46:37 +0530

    for loops

---
+++ Control Flow/forLoop.py
@@ -0,0 +1,6 @@
+for number in range(3):
+    print(number + 1, "Attempt", (number + 1) * "*")
+
+# 1 Attempt *
+# 2 Attempt * *
+# 3 Attempt * * *
---
+++ Control Flow/forLoop2.py
@@ -0,0 +1,10 @@
+for number in range(1, 10, 2):
+    print(number, "Attempt", (number) * "*")  # no need to write number+1
+
+# The third argument is the step
+
+# 1 Attempt *
+# 3 Attempt ** *
+# 5 Attempt ** ***
+# 7 Attempt ** *****
+# 9 Attempt ** *******
