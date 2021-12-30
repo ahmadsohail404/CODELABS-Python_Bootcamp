9871e8436e3c86bbc1ed2828a071d64a8d7084d1
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-31 02:56:12 +0530

    cost of raising exceptions in terms of runtime

---
+++ Exceptions/07cost_of_raising_exc.py
@@ -0,0 +1,30 @@
+from timeit import timeit
+
+code1 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10/age
+
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    pass #using it here because we can't leave the clause empty
+"""
+
+code2 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10/age
+
+
+x_factor = calculate_xfactor(-1)
+if x_factor == None:
+    pass
+
+"""
+
+print("first code: ", timeit(code1, number=10000))
+print("second code: ", timeit(code2, number=10000))
