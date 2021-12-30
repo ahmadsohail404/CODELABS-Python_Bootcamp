b47d76678317112e020b57eb8744f4ef84009031
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-30 20:08:25 +0530

    custom interactive shell

---
+++ Control Flow/whileLoop.py
@@ -0,0 +1,10 @@
+# number = 100
+# while number > 0:
+#     print(number)
+#     number //= 2
+
+# sort of custom python interactive shell
+command = ""
+while command.lower() != "quit":
+    command = input(">")
+    print("ECHO", command)
