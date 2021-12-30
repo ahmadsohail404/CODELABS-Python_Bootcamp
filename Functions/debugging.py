62fc1a33a34dc34b553ffed80060e37bbf64fb98
Sohail Ahmad <sohailss2412@gmail.com>
2021-12-31 01:35:21 +0530

    debugging

---
+++ Functions/debugging.py
@@ -0,0 +1,9 @@
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+        return total   # knowingly writing along in indentation for debugging
+
+
+print("start")
+print(multiply(2, 3, 4))
